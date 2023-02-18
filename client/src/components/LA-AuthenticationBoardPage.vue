348eb48bf8e16c8bb7128d03810358a05b56c570
nyhwbh <nam18pt@gmail.com>
2023-02-19 05:27:27 +0900

    add page structures

---
+++ client/src/components/LA-AuthenticationBoardPage.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <div class="tabs">
+            <div :class="{ active: activeTab === 'record' }" @click="changeTab('record')">Tab 1</div>
+            <div :class="{ active: activeTab === 'user' }" @click="changeTab('user')">Tab 2</div>
+        </div>
+        <div class="content">
+            <div v-if="activeTab === 'record'">Tab 1 Content</div>
+            <div v-if="activeTab === 'user'">Tab 2 Content</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            activeTab: 'record',
+        };
+    },
+    methods: {
+        changeTab(tab) {
+            this.activeTab = tab;
+        },
+    },
+};
+</script>
+
+<style>
+.tabs {
+    display: flex;
+    background-color: #eee;
+}
+
+.tabs div {
+    flex: 1;
+    text-align: center;
+    padding: 10px 0;
+    cursor: pointer;
+}
+
+.tabs .active {
+    background-color: #fff;
+}
+
+.content {
+    padding: 20px;
+}
+</style>
---
+++ client/src/components/LA-LoginJoinin.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <div class="tabs">
+            <div :class="{ active: activeTab === 'logIn' }" @click="changeTab('logIn')">Tab 1</div>
+            <div :class="{ active: activeTab === 'joinIn' }" @click="changeTab('joinIn')">Tab 2</div>
+        </div>
+        <div class="content">
+            <div v-if="activeTab === 'logIn'">Tab 1 Content</div>
+            <div v-if="activeTab === 'joinIn'">Tab 2 Content</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            activeTab: 'logIn',
+        };
+    },
+    methods: {
+        changeTab(tab) {
+            this.activeTab = tab;
+        },
+    },
+};
+</script>
+
+<style>
+.tabs {
+    display: flex;
+    background-color: #eee;
+}
+
+.tabs div {
+    flex: 1;
+    text-align: center;
+    padding: 10px 0;
+    cursor: pointer;
+}
+
+.tabs .active {
+    background-color: #fff;
+}
+
+.content {
+    padding: 20px;
+}
+</style>
